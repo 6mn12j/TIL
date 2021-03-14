fbbf4b5c4c16cda265c06af3ce5a32e6ab4cda1a
6mn12j <dbwk0404@hanmail.net>
2021-03-15 02:45:08 +0900

    유튜브 클론코딩 기능 구현

--- html/app.js
+++ html/app.js
@@ -1,18 +1,28 @@
 const nextItemButton = document.querySelector('button .next__item i');
 const nextItems = document.querySelectorAll('.next__item');
+const titleButton = document.querySelector('.title__button');
 
 const HandlenextItemMouseIn = (e) => {
   const targetItem = e.target.closest('li');
   const targetButton = targetItem.querySelector('.next__item i');
   targetButton.classList.remove('blind');
 };
+
 const HandlenextItemMouseOut = (e) => {
   const targetItem = e.target.closest('li');
   const targetButton = targetItem.querySelector('.next__item i');
   targetButton.classList.add('blind');
 };
 
+const titleButtonClick = (e) => {
+  const targetTitle = e.target.closest('button').closest('.title').querySelector('.title__text');
+
+  targetTitle.style.whiteSpace = targetTitle.style.whiteSpace === 'normal' ? 'nowrap' : 'normal';
+};
+
 for (const item of nextItems) {
   item.addEventListener('mouseover', HandlenextItemMouseIn);
   item.addEventListener('mouseout', HandlenextItemMouseOut);
 }
+
+titleButton.addEventListener('click', titleButtonClick);
